018c1ba9cbdacf649b8870f3d02751c363615d2e
Viktor <doctorTardis96@yandex.ru>
2022-10-03 12:27:43 +1000

    убрал еще одну проблему

---
+++ FirstTask/FirstTask.cs
@@ -0,0 +1,18 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
+//а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое значение: ");
+var n1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе значение: ");
+            var n2 = int.Parse(Console.ReadLine());
+            if ( n1 > n2 ){
+                 Console.Write("Максимальное значение введенных чисел найдено: ");
+                Console.WriteLine( n1 );
+            }
+            else {
+                Console.Write("Максимальное значение введенных чисел найдено: ");
+                Console.WriteLine( n2 );
+            }
---
+++ SecondTask/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход
+//три числа и выдаёт максимальное из этих чисел.
+
+
+
+Console.Write("Введите первое значение: ");
+var n1 = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе значение: ");
+    var n2 = int.Parse(Console.ReadLine());
+        Console.Write("Введите третье значение: ");
+        var n3 = int.Parse(Console.ReadLine());
+int max = n1;      
+if ( n2 > max ) max = n2;
+if ( n3 > max ) max = n3;
+Console.Write("Максимальное значение из введенных чисел: ");
+Console.WriteLine(max);
+            
+    
+           
+   
+
+                
