f300777ee85b9e2e7cfe3373711a3cf5bb51bf44
Viktor <doctorTardis96@yandex.ru>
2022-10-05 16:02:05 +1000

    Программа для дней недели

---
+++ seventask/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+void EasyMethod()
+{
+    Console.Write("Введите нумерацию дня ");
+    var day = int.Parse(Console.ReadLine());
+    if (day == 6)
+    {
+        Console.Write("Выходной день:Суббота");
+    }
+    if (day == 7)
+    {
+        Console.Write("Выходной день:Воскресенье");
+    }
+    if (day == 5)
+    {
+        Console.Write("Рабочий день");
+    }
+    if (day == 4)
+    {
+        Console.Write("Рабочий день");
+    }
+    if (day == 3)
+    {
+        Console.Write("Рабочий день");
+    }
+    if (day == 2)
+    {
+        Console.Write("Рабочий день");
+    }
+    if (day == 1)
+    {
+        Console.Write("Рабочий день");
+    }
+}
+
+EasyMethod();
