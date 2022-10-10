235a4c90faf39c40b2024349fdbf3dcb0c411539
Viktor <doctorTardis96@yandex.ru>
2022-10-11 01:58:03 +1000

    Палиндром

---
+++ EighthTask/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+int num, temp, revNum = 0, rem;
+Console.WriteLine("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;
+while (num > 0)
+{
+    rem = num % 10;
+    revNum = revNum * 10 + rem;
+    num = num / 10;
+}
+if (revNum == temp)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Это не палиндром");
+}
+
