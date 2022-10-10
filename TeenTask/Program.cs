55a6951f88d8a28362098f44fe8a3b1152f0fda5
Viktor <doctorTardis96@yandex.ru>
2022-10-11 04:33:02 +1000

    Степень

---
+++ TeenTask/Program.cs
@@ -0,0 +1,15 @@
+﻿degree();
+
+void degree()
+{
+    double a;
+    double b;
+    Double c;
+    Console.WriteLine("Введите число: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите степень: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    c = Math.Pow(a, b);
+    Console.WriteLine("Число " + a + " в степени " + b + " равно " + c + "!");
+}
+
