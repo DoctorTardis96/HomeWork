27768577a39caa071314d50f9744b94e782cde6b
Viktor <doctorTardis96@yandex.ru>
2022-10-12 03:40:12 +1000

    Создать 8 рандомных чиел и вывести их

---
+++ twelfthtask/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+/*         Console.Write("Введите колличество элементов массива:\t");
+        
+        int n = int.Parse(Console.ReadLine()); */
+        
+        int[] array = new int[8];
+        Random rand = new Random();
+        for (int i = 0; i < 8; i++)
+        {
+            array[i] = rand.Next(0, 8);
+        }
+         
+/*         for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write ($"\nВведите элемент массива под индексом {i}:\t ");
+            array[i] = int.Parse(Console.ReadLine());
+        } */
+        Console.WriteLine("\nВывод массива:");
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]+ ",");
+        }
+    }
+
+
+
+}
