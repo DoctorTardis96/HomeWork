58d0028a29fb89dfeb05b2607eace4a9d964d5c3
Viktor <doctorTardis96@yandex.ru>
2022-10-12 04:27:00 +1000

    возвести в степень без math.pow

---
+++ 13.task/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+static void MyMethod()
+{
+    Console.WriteLine("\nВведите число которое хотите возвести в степень:\t");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("\nВведите степень, в которую хотите возвести число:\t");
+    int b = int.Parse(Console.ReadLine());
+    int i = 0;
+    int sum = 1;
+    while(i < b)
+    {
+        i++;
+        sum *= a;
+        
+    }
+    Console.WriteLine(sum);
+}
+MyMethod();
