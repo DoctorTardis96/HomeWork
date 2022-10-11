f1444e55cd2bba2c354e83c3ca39c22bdf32edcc
Viktor <doctorTardis96@yandex.ru>
2022-10-12 01:25:12 +1000

    Сумма чисел

---
+++ eleventhtask/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void MyMethod()
+{
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    Console.WriteLine(sum);
+}
+MyMethod();
